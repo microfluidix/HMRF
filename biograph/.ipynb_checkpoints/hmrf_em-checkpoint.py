1efca0d786be61e471eaa3d2f55f2fdb5c309d34
Valentin <valentin.bonnet@polytechnique.edu>
2021-04-30 16:24:44 +0200

    Functional algorithm

--- biograph/.ipynb_checkpoints/hmrf_em-checkpoint.py
+++ biograph/.ipynb_checkpoints/hmrf_em-checkpoint.py
@@ -26,15 +26,15 @@ class hmrf():
                  G,
                  K = 5,
                  beta = 1,
-                 epsilon = 0.0001):
+                 max_it = 50):
         
         self.graph = G
         self.K = K
         self.beta = beta
-        self.epsilon = epsilon
+        self.max_it = max_it
 
         self.mu = []
-        self.cov = []
+        self.sigma2 = []
 
         self.node_attributes = np.unique(np.array([list(self.graph.nodes[n].keys()) for n in self.graph.nodes()]).flatten())
 
@@ -45,7 +45,7 @@ class hmrf():
         self.cell_types = np.unique([cell_types[node] for node in cell_types.keys()])
         self.number_of_cell_types = len(self.cell_types)
 
-        self.color_list= color_list = [plt.cm.Set3(i) for i in range(self.K)]
+        self.color_list = [plt.cm.Set3(i) for i in range(self.K)]
         
     
     def initiate_model(self):
@@ -68,124 +68,125 @@ class hmrf():
             
         self.graph = G
         
-        # initialisation
-        cl = categorical_vector(self.graph, 'class')
-        ct = categorical_vector(self.graph, 'cell_type')
-        for j in range(self.K):
-            cell_type_j = ct[cl == j]
-            self.mu.append(np.mean(cell_type_j))
-            self.cov.append(np.cov(cell_type_j))
-             
+        # initialisation of parameters
+        self.mu, self.sigma2 = self.update_parameters()
         
-    def expectation_step(self):
+    def update_labels(self):
         
-        G = self.graph
+        # Important quantities
+        cell_class_dict = nx.get_node_attributes(self.graph, 'class') # dict of cell labels
 
-        cell_class_dict = nx.get_node_attributes(G, 'class')
+        cell_type_list = categorical_vector(self.graph, 'cell_type') # list of cell types
 
-        P_neigh = np.zeros((len(G.nodes), self.K))
+        N = len(cell_type_list) # Number of cell
 
-        for node in G.nodes:
+        T = len(np.unique(cell_type_list)) # Number of cell types
 
-            neighbour_cell_class = [cell_class_dict[n] for n in G.neighbors(node)]
+        # Create matrix from cell type
+        mat_cell_type = np.zeros((N, T))
+        for i in range(N):
+            mat_cell_type[i, cell_type_list[i]] = 1
+        
+        # Influence of neighbors labels
+
+        log_P_neigh = np.zeros((len(self.graph.nodes), self.K))
+
+        for node in self.graph.nodes:
+
+            neighbour_cell_class = [cell_class_dict[n] for n in self.graph.neighbors(node)]
             neighbour_cell_class_counter = Counter(neighbour_cell_class)
 
             for cell_class in neighbour_cell_class_counter.keys():
 
                 for k in range(self.K):
-                    P_neigh[node, k] += self.beta*int(k == cell_class)*neighbour_cell_class_counter[cell_class]
+                    log_P_neigh[node, k] += self.beta*int(k == cell_class)*neighbour_cell_class_counter[cell_class]
+                    
+        # Emission log-probability
 
-        P_gauss = np.zeros((len(G.nodes), self.K))
-
-        cell_type_dict = nx.get_node_attributes(G, 'cell_type')
-
-        for node in G.nodes:
-
-            x = cell_type_dict[node]
-
-            for j in range(self.K):
-                pref = 1/np.sqrt(2*np.pi*self.cov[j])
-
-                P_gauss[node, j] = pref*np.exp(-0.5*((x-self.mu[j])**2)/self.cov[j])
-
-        # calculate product proba
-
-        prod_proba = P_gauss * P_neigh
-
-        # calculate gamma proba
-
-        gamma = np.zeros((len(G.nodes), self.K))
+        log_P_gauss = np.zeros((len(self.graph.nodes), self.K))
 
         for j in range(self.K):
-            gamma[:, j] = prod_proba[:, j]/np.sum(prod_proba,1)
-            
-        return gamma
-    
-    
-    def maximisation_step(self):
-        
-        gamma = self.expectation_step()
-        
-        G = self.graph
-        
-        new_class = np.argmax(gamma, axis=1)
+            var = self.sigma2[j]
+            for i in range(N):
+                xi = mat_cell_type[i]
+                for t in range(T):
+                    a = (-0.5*(xi[t]-self.mu[j,t])**2)/var[t, t]
+                    if ~np.isnan(a):
+                        if a == -np.inf:
+                            log_P_gauss[i, j] += -1e10
+                        else:
+                            log_P_gauss[i, j] += a
+                            
+        # MAP criterion to determine new labels
 
-        for node in sorted(G.nodes):
-            nx.set_node_attributes(G, {node:new_class[node]}, 'class')
-            nx.set_node_attributes(G, {node:self.color_list[new_class[node]]}, 'color')
-            nx.set_node_attributes(G, {node:new_class[node]}, 'legend')
-            
-        self.graph = G
-        
-        ct = categorical_vector(self.graph, 'cell_type')
-        
-        n = np.sum(gamma, axis = 0)
+        sum_prob = log_P_gauss + log_P_neigh
 
-        new_mu = np.dot(gamma.T, ct)/n
-
-        new_cov = np.zeros(self.K)
+        new_class = np.argmax(sum_prob, axis=1)
         
+        # Update labels in the graph
+
+        for node in sorted(self.graph.nodes):
+            nx.set_node_attributes(self.graph, {node:new_class[node]}, 'class')
+            nx.set_node_attributes(self.graph, {node:self.color_list[new_class[node]]}, 'color')
+            nx.set_node_attributes(self.graph, {node:new_class[node]}, 'legend')
+
+    def update_parameters(self):
+
+        # List of cell types
+        cell_type_list = categorical_vector(self.graph, 'cell_type')
+
+        # Number of cells
+        N = len(cell_type_list)
+
+        # Number of cell types
+        T = len(np.unique(cell_type_list))
+
+        # Create matrix from cell type
+        mat_cell_type = np.zeros((N, T))
+        for i in range(N):
+            mat_cell_type[i, cell_type_list[i]] = 1
+
+        # List of labels
+        cell_class_list = categorical_vector(self.graph, 'class')
+
+        # Number of cell labels
+        classes, card_classes = np.unique(cell_class_list, return_counts=True)
+
+        # Little trick to allow loosing some labels
+        card_classes2 = np.zeros(self.K)
+        card_classes2[classes] = card_classes
+        card_classes = card_classes2
+
+        # Count frequencies of cell types in each latent class
+        freq = np.zeros((self.K, T))
+
+        # Variability inside class
+        sig = [np.eye(T) for j in range(self.K)]
+
         for j in range(self.K):
-            for i in range(len(ct)):
-                new_cov[j] += gamma[i, j]*((ct[i]-new_mu[j])**2)
-        new_cov /= n
+
+            # Count number of cells of each type in class j
+            x1, x2 = np.unique(cell_type_list[cell_class_list == j], return_counts=True)
+            freq[j, x1] = x2
+            freq[j, :] /= card_classes[j]
+            freq[j, :] /= np.sum(freq[j, :])
+
+            # Compute variability inside class j
+            cell_type_in_j = mat_cell_type[cell_class_list == j]
+            sig_j = np.sum((cell_type_in_j - freq[j])**2, axis = 0)
+            sig_j /= card_classes[j]
+            sig[j] *= sig_j
+
+        return freq, sig
         
-        return new_mu, new_cov
-    
+             
     def run(self):
-        
-        stopcrit_mu = self.epsilon + 1
-        stopcrit_cov = self.epsilon + 1
-        
-        cpt = 0
-        print(cpt)
-        
-        while (stopcrit_mu >= self.epsilon) or (stopcrit_cov >= self.epsilon):
-            new_mu, new_cov = self.maximisation_step()
-            
-            copy_stopcrit_mu = np.copy(stopcrit_mu)
-            copy_stopcrit_cov = np.copy(stopcrit_cov)
-            
-            # Stopping criteria
-            stopcrit_mu = np.max(np.abs(new_mu - self.mu))/(1 + np.max([new_mu, self.mu]))
-            stopcrit_cov = np.max(np.abs(new_cov - self.cov))/(1 + np.max([new_cov, self.cov]))
-            print(stopcrit_mu, stopcrit_cov, cpt)
-            
-            self.mu = new_mu
-            self.cov = new_cov
-            
-            cpt += 1
-            
-            if round(float(copy_stopcrit_mu), 6) == round(stopcrit_mu, 6):
-                if round(float(copy_stopcrit_cov), 6) == round(stopcrit_cov, 6):
-                    print('stationnary state : convergence issue')
-                    break
-            
-            if cpt > 100:
-                break
-        
-        print(cpt)
-            
+        list_param = [[self.mu, self.sigma2]]
+        for cpt in tqdm([i for i in range(self.max_it)]):
+            self.update_labels()
+            self.mu, self.sigma2 = self.update_parameters()
+            list_param.append([self.mu, self.sigma2])
+        return list_param
         
 def categorical_vector(G, category):
     
